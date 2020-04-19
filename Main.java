2a3a82838f50c73c3005c3b0a2e8b645b512cff1
Viki4325 <58233681+Viki4325@users.noreply.github.com>
2020-04-18 19:51:13 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,4 @@
 //-----------------------------------------
-// NAME		: Makadia Viki Suresh
-// STUDENT NUMBER	: 7870989
-// COURSE		: COMP 2150
-// INSTRUCTOR	: your instructor
-// ASSIGNMENT	: assignment 3
-// QUESTION	: question 1
-//
 // REMARKS: It is an implementation of the connect 4 game. There is an AI player which is implemented in brute force way.
 //
 //
